689a229774f72cb626d0de2b9eca0dc802a35aea
Trần Cao Minh Bách <45133811+bachtran2000@users.noreply.github.com>
2021-08-10 15:27:44 +0700

    run public

--- www/lab.py
+++ www/lab.py
@@ -11,7 +11,8 @@ bp = Blueprint('lab', __name__)
 
 INVALID_KEY = '-1'
 
-app.run(host="0.0.0.0",port=5000,debug=True)
+app.run(host="0.0.0.0",port=1210,debug=True)
+
 @bp.route('/')
 def main():
     # set debug to 1 and show the verbose output
